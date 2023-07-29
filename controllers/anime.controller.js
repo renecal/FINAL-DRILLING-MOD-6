151a09043f6e83cc5ce46a44bd0d9f56fa368e0e
renecal <rene.calfin@gmail.com>
2023-07-29 16:21:22 -0400

    try catch al metodo findall y respuesta json con code y message

--- controllers/anime.controller.js
+++ controllers/anime.controller.js
@@ -1,3 +1,9 @@
+
 export const findAll = (req, res) => {
- res.send("todos los animes");
+    try {
+        res.status(200).json({ code: 200, message: "ok"});
+        
+    } catch (error) {
+        res.status(500).json({ code: 500, message: "Error al leer los datos solicitados de anime"});
+    }
 }
