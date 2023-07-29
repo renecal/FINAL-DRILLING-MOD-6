92b387c90b5837751e3fe121956a36ae69504ec3
renecal <rene.calfin@gmail.com>
2023-07-29 16:24:22 -0400

    importando fs manejo de archivos, path y url para dirname

--- controllers/anime.controller.js
+++ controllers/anime.controller.js
@@ -1,6 +1,17 @@
+import fs from "fs/promises";  // manejo de archivos asincrono con promesas
+import * as path from "path";  //donde estan las rutas
+import { fileURLToPath } from "url"; // obtiene la informacion de la url
+const __dirname = path.dirname(fileURLToPath(import.meta.url)); 
+//meta.url manejo de carpetas y archivos
 
-export const findAll = (req, res) => {
+// __dirname hace referencia al directorioi del archivo que ocupamos actualmente
+
+let pathAnime = path.resolve(__dirname, "../database/anime.json");
+
+export const findAll = async (req, res) => {
     try {
+        let animes = await fs.readFile(pathAnime, 'utf-8')
+        console.log(animes)
         res.status(200).json({ code: 200, message: "ok"});
         
     } catch (error) {
