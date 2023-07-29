c36b0fc00c2f64bfb6768b6c91604198eff1103c
renecal <rene.calfin@gmail.com>
2023-07-29 17:19:19 -0400

    metodo createAnime ok

--- controllers/anime.controller.js
+++ controllers/anime.controller.js
@@ -3,6 +3,7 @@ import * as path from "path"; //donde estan las rutas
 import { fileURLToPath } from "url"; // obtiene la informacion de la url
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 //meta.url manejo de carpetas y archivos
+import { v4 as uuid } from "uuid";
 
 // __dirname hace referencia al directorioi del archivo que ocupamos actualmente
 
@@ -63,27 +64,30 @@ export const createAnime = async (req, res) => {
   try {
     let data = await fs.readFile(pathAnime, "utf-8");
     data = JSON.parse(data);
-    let { id, nombre, genero, anho, autor } = req.body;
+    let { nombre, genero, anho, autor } = req.body;
     // console.log(nombre, genero)
     let nuevoAnime = {
-        id,
+        id: uuid().slice(0,6),
         nombre,
         genero,
         anho,
         autor
     }
-    res.status(200).json({
-      code: 200,
-      message: "ok",
-      animes: data.anime,
-    });
+    data.anime.push(nuevoAnime);
+    await fs.writeFile(pathAnime, JSON.stringify(data, null, 2), "utf8");
+
+    res.status(201).json({
+        code: 201,
+        message: `Se ha creado con exito el anime con id ${nuevoAnime.id}`,
+        animal: nuevoAnime
+});
   } catch (error) {
     console.log(error);
     res
       .status(500)
       .json({
         code: 500,
-        message: "Error al leer los datos solicitados de anime",
+        message: "Error al crear anime",
       });
   }
 };
